206cd1f43120e56231939a986f900091a96c3df4
Nilou <desnillight@gmail.com>
2024-10-08 14:15:14 +0330

    debugging menu opening

--- scripts/my-javascript.js
+++ scripts/my-javascript.js
@@ -1,6 +1,6 @@
 $('a#top-header__open-close').click(function () {
-    $('#overlay-menu').toggleClass('show');
+    $('#overlay-menu').addClass('show');
 })
 $('#overlay-menu i.fa-close').click(function () {
-    $('#overlay-menu').toggleClass('close');
+    $('#overlay-menu').removeClass('show');
 })
