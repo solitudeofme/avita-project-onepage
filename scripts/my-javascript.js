36cdeade8de0b2f7fc264e813871507d6e7f5895
Nilou <desnillight@gmail.com>
2025-02-02 00:01:31 +0330

    updating the project

--- scripts/my-javascript.js
+++ scripts/my-javascript.js
@@ -4,3 +4,12 @@ $('a#top-header__open-close').click(function () {
 $('#overlay-menu i.fa-close').click(function () {
     $('#overlay-menu').removeClass('show');
 })
+function updatePlaceholder() {
+  const input = document.querySelector("#search-input");
+
+  if (window.innerWidth <= 720) {
+    input.setAttribute("placeholder", "جست و جو در خدمات");
+  } 
+}
+window.addEventListener("load", updatePlaceholder);
+window.addEventListener("resize", updatePlaceholder);
